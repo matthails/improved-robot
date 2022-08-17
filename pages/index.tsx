f9396347b131dd76264268372f76cd29305f0321
Matt <32686182+matthails@users.noreply.github.com>
2022-08-17 17:11:23 +0100

    a commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@
 const Home = () => {
   return (
    <div className="w-screen h-screen flex flex-col justify-center items-center">
-    <h1 className="text-6xl font-bold font-pacifico">Hello, World!</h1>
+    <h1 className="text-6xl font-bold">Hello, World!</h1>
     <p className="text-2xl">Lorum ipsum ...</p>
    </div>
   )
